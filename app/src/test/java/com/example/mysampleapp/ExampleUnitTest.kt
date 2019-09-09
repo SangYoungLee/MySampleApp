2b0b207581ac5b81819940f4e7f40b7641e6a43b
SangYeongLee <lsy2014@gmail.com>
2019-09-09 21:34:08 +0900

    Soft Keyword 변수명 적용 테스트

--- app/src/test/java/com/example/mysampleapp/ExampleUnitTest.kt
+++ app/src/test/java/com/example/mysampleapp/ExampleUnitTest.kt
@@ -14,4 +14,10 @@ class ExampleUnitTest {
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
+
+    @Test
+    fun softKeywordTest() {
+        val by = 2
+        assertEquals(by, 2)
+    }
 }
