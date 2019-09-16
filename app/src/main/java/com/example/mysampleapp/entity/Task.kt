3f1ff9a400f3b9af8c5adcb9b3dd3646e936077e
SangYeongLee <lsy2014@tmon.co.kr>
2019-09-16 18:17:13 +0900

    - Task Class에 Entity 적용 안되어있는 문제 수정

--- app/src/main/java/com/example/mysampleapp/entity/Task.kt
+++ app/src/main/java/com/example/mysampleapp/entity/Task.kt
@@ -1,7 +1,13 @@
 package com.example.mysampleapp.entity
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.util.*
+
+@Entity(tableName = "tasks")
 data class Task(
-    val title: String,
-    val contents: String,
-    val id: String? = ""
+    @ColumnInfo(name = "title") val title: String,
+    @ColumnInfo(name = "contents") val contents: String,
+    @PrimaryKey @ColumnInfo(name = "taskId") val id: String? = UUID.randomUUID().toString()
 )
