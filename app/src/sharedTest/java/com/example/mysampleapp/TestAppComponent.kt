3a2560dad5a36c2c1b91cca1a72b9a94af3569b1
SangYeongLee <lsy2014@tmon.co.kr>
2019-09-24 15:20:43 +0900

    - TestAppComponent 추가 안되어있던 문제 수정

---
+++ app/src/sharedTest/java/com/example/mysampleapp/TestAppComponent.kt
@@ -0,0 +1,30 @@
+package com.example.mysampleapp
+
+import android.content.Context
+import com.example.mysampleapp.activity.FragmentModuleBinds
+import com.example.mysampleapp.application.AppModule
+import com.example.mysampleapp.application.AppModuleBinds
+import com.example.mysampleapp.application.TodoApplication
+import com.example.mysampleapp.base.viewmodel.ViewModelModuleBinds
+import com.example.mysampleapp.repository.ITaskRepository
+import dagger.BindsInstance
+import dagger.Component
+import dagger.android.AndroidInjector
+import dagger.android.support.AndroidSupportInjectionModule
+import javax.inject.Singleton
+
+/**
+ * Created By lsy2014 on 2019-09-24
+ */
+@Singleton
+@Component(modules = [AndroidSupportInjectionModule::class, AppModule::class,
+    AppModuleBinds::class, FragmentModuleBinds::class, ViewModelModuleBinds::class])
+interface TestAppComponent : AndroidInjector<TodoApplication> {
+
+    @Component.Factory
+    interface Factory {
+        fun create(@BindsInstance context: Context): TestAppComponent
+    }
+
+    val taskRepository: ITaskRepository
+}
